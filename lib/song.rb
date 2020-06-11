5943b2ce3e073e0b2151301744c9901f5797ce30
Elijah Kirschen <ekirschen584@gmail.com>
2020-06-11 19:11:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,3 +34,26 @@ class Song
     name.downcase.gsub(' ', '-')
   end
 end
+
+class Song
+
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
+
+  attr_accessor :name
+  attr_reader :artist
+
+  @@songs = []
+
+  def self.all
+    @@songs
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self) unless artist.songs.include?(self)
+  end
+  
+end
